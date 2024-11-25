373c61e75f927797eca4c6b7a5f910ae0bd21484
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-26 00:25:07 +0900

    refactor: 아웃풋뷰를 이용하여 출력값 재설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import InputView from "./views/InputView.js";
 import OutputView from "./views/OutputView.js";
 import Validator from "./utils/Validator.js";
-import { MissionUtils, Console } from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
 import { OUTPUT_MESSAGE } from "./constants/outputMessage.js";
 import RandomNum from "./utils/RandomNum.js";
 
@@ -19,25 +19,24 @@ class App {
       let strike = 0;
       let ball = 0;
       let notsing = 0;
+
       //컴퓨터와 나의 값 비교
       for (let i = 0; i < numArr.length; i++) {
         if (random.includes(numArr[i])) {
-          console.log("2", random);
-
           if (random.indexOf(numArr[i]) === numArr.indexOf(numArr[i])) {
             strike += 1;
           } else ball += 1;
         } else notsing += 1;
       }
 
-      if (notsing === 3) Console.print(OUTPUT_MESSAGE.NOTSING);
+      if (notsing === 3) OutputView.printNothing();
       if (strike > 0 && ball > 0) {
-        Console.print(`${strike}스트라이크 ${ball}볼`);
-      } else if (strike > 0) Console.print(`${strike}스트라이크`);
-      else if (ball > 0) Console.print(`${ball}볼`);
+        OutputView.printRightBallStrike(ball, strike);
+      } else if (strike > 0) OutputView.printRightStrike(strike);
+      else if (ball > 0) OutputView.printRightBall(ball);
 
       if (strike === 3) {
-        Console.print(OUTPUT_MESSAGE.GET_NUM_RIGHT);
+        OutputView.printRightAllNum();
         const retry = await InputView.readretry();
 
         Validator.retryNumValidation(retry);
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -5,6 +5,28 @@ const OutputView = {
   printStartBaseballGame() {
     Console.print(OUTPUT_MESSAGE.START_GAME);
   },
+
+  printRightBallStrike(ball, strike) {
+    Console.print(
+      `${ball}${OUTPUT_MESSAGE.BALL} ${strike}${OUTPUT_MESSAGE.STRIKE}`
+    );
+  },
+
+  printRightStrike(strike) {
+    Console.print(`${strike}${OUTPUT_MESSAGE.STRIKE}`);
+  },
+
+  printRightBall(ball) {
+    Console.print(`${ball}${OUTPUT_MESSAGE.BALL}`);
+  },
+
+  printNothing() {
+    Console.print(OUTPUT_MESSAGE.NOTSING);
+  },
+
+  printRightAllNum() {
+    Console.print(OUTPUT_MESSAGE.GET_NUM_RIGHT);
+  },
 };
 
 export default OutputView;
