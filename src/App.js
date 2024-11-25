3da840251f0127232c0d06a9bb3c3ec52df23476
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-25 22:10:58 +0900

    feat: 야구게임 숫자 값 판정 로직 추가
    - 같은 자리에 있으면 => 스트라이크 - 같은 수를 입력했지만 다른 자리에 있으면 => 볼
    - 같은 수가 없으면 => 낫싱

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import InputView from "./views/InputView.js";
 import OutputView from "./views/OutputView.js";
 import Validator from "./utils/Validator.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+import { OUTPUT_MESSAGE } from "./constants/outputMessage.js";
+import RandomNum from "./utils/RandomNum.js";
 
 class App {
   async play() {
     OutputView.printStartBaseballGame();
 
-    const computer = [];
-    while (computer.length < 3) {
-      const randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(randomNum)) {
-        computer.push(randomNum);
-      }
-    }
-
+    let random = RandomNum.createRandomNum();
     while (true) {
       const num = await InputView.readBaseballNum();
 
       const numArr = num.split("").map(Number);
       Validator.baseballNumValidation(numArr);
+
+      let strike = 0;
+      let ball = 0;
+      let notsing = 0;
+      //컴퓨터와 나의 값 비교
+      for (let i = 0; i < numArr.length; i++) {
+        if (random.includes(numArr[i])) {
+          console.log("2", random);
+
+          if (random.indexOf(numArr[i]) === numArr.indexOf(numArr[i])) {
+            strike += 1;
+          } else ball += 1;
+        } else notsing += 1;
+      }
+
+      if (notsing === 3) Console.print(OUTPUT_MESSAGE.NOTSING);
+      if (strike > 0 && ball > 0) {
+        Console.print(`${strike}스트라이크 ${ball}볼`);
+      } else if (strike > 0) Console.print(`${strike}스트라이크`);
+      else if (ball > 0) Console.print(`${ball}볼`);
     }
   }
 }
-
-const app = new App();
-await app.play();
 
 export default App;
