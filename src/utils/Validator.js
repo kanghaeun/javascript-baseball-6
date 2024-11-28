c356a0b308e63cf4e52e41efe08981e28b50a15e
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-28 22:56:15 +0900

    fix: some 메서드 활용하여 계속 false였던 유효성 검사 수정

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -4,7 +4,9 @@ class Validator {
   static baseballNum(numArr) {
     if (numArr.length === 0) throw new Error(ERROR_MESSAGE.EMPTY_INPUT);
     if (numArr.length !== 3) throw new Error(ERROR_MESSAGE.BASEBALL_NUM_FORMAT);
-    if (numArr === isNaN) throw new Error(ERROR_MESSAGE.BASEBALL_NUM_FORMAT);
+    // if (numArr === isNaN) throw new Error(ERROR_MESSAGE.BASEBALL_NUM_FORMAT);
+    if (numArr.some((num) => isNaN(num)))
+      throw new Error(ERROR_MESSAGE.BASEBALL_NUM_FORMAT);
 
     let setNumArr = new Set(numArr);
     if (numArr.length !== setNumArr.size)
