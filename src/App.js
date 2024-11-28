c77e42c06c7f0524fc8079723d021a521d9ac95f
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-28 13:51:25 +0900

    refactor: 랜덤 값 추출 로직 mvc 패턴 적용

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
-import InputView from "./views/InputView.js";
-import OutputView from "./views/OutputView.js";
-import Validator from "./utils/Validator.js";
-import { Console } from "@woowacourse/mission-utils";
-import { OUTPUT_MESSAGE } from "./constants/outputMessage.js";
-import RandomNum from "./utils/RandomNum.js";
-
+import Computer from "./models/Computer.js";
+import MatchChecker from "./models/MatchChecker.js";
+import BaseballGame from "./controller/BaseballGame.js";
 class App {
+  #computer;
+  #matchResult;
+  #gamecontroller;
+
+  constructor() {
+    this.#computer = new Computer();
+    this.#matchResult = new MatchChecker();
+    this.#gamecontroller = new BaseballGame(this.#computer, this.#matchResult);
+  }
   async play() {
-    OutputView.printStartBaseballGame();
-
-    let random = RandomNum.createRandomNum();
-    while (true) {
-      const num = await InputView.readBaseballNum();
-
-      const numArr = num.split("").map(Number);
-      Validator.baseballNumValidation(numArr);
-
-      let strike = 0;
-      let ball = 0;
-      let notsing = 0;
-
-      //컴퓨터와 나의 값 비교
-      for (let i = 0; i < numArr.length; i++) {
-        if (random.includes(numArr[i])) {
-          if (random.indexOf(numArr[i]) === numArr.indexOf(numArr[i])) {
-            strike += 1;
-          } else ball += 1;
-        } else notsing += 1;
-      }
-
-      if (notsing === 3) OutputView.printNothing();
-      if (strike > 0 && ball > 0) {
-        OutputView.printRightBallStrike(ball, strike);
-      } else if (strike > 0) OutputView.printRightStrike(strike);
-      else if (ball > 0) OutputView.printRightBall(ball);
-
-      if (strike === 3) {
-        OutputView.printRightAllNum();
-        const retry = await InputView.readretry();
-
-        Validator.retryNumValidation(retry);
-
-        if (retry === "2") return;
-        random = RandomNum.createRandomNum();
-      }
-    }
+    // await this.#gamecontroller.start();
   }
 }
+
+const app = new App();
+await app.play();
 
 export default App;
--- src/models/Computer.js
+++ src/models/Computer.js
@@ -1,6 +1,7 @@
-// 랜덤 숫자를 만드는 컴퓨터 역할 (랜덤 수 생성)
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 class Computer {
+  // 랜덤 숫자를 만드는 컴퓨터 역할 (랜덤 수 생성)
   #randomNumArr;
 
   constructor() {
@@ -21,6 +22,7 @@ class Computer {
   #getRandomNum() {
     const randomNumArr = this.#setRandomNum();
     this.#randomNumArr = randomNumArr;
+    console.log(this.#randomNumArr);
   }
 }
 
